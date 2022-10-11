ad5d08313925dc079c2b50793603f241775c4ec2
1716250001 <1716250001@qq.com>
2022-10-11 20:47:12 +0800

    Create hello_world.go
    first

---
+++ demo1/hello_world.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	println("Hello World!")
+	fmt.Println("Hello" + "luhanmin")
+
+	var age int
+	age = 23
+	println("luhanmin is ", age)
+
+	s := fmt.Sprintf("%d -> luhanmin", age)
+	println(s)
+
+	var year, month, day int
+	year = 2022
+	month = 4
+	day = 12
+	println(fmt.Sprintf("luhanmin born in %d %d %d", year, month, day))
+}
